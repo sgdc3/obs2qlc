699d1f73379a9bdff867a886747f48cd103d7eeb
Gabriele C <sgdc3.mail@gmail.com>
2019-05-23 10:47:43 +0200

    Codestyle

--- index.js
+++ index.js
@@ -16,125 +16,125 @@ var obsSocket;
 var obsConnected = false;
 
 function initializeObsSocket() {
-	obsSocket = new WebSocket(obsWsAddress);
+    obsSocket = new WebSocket(obsWsAddress);
 
-	obsSocket.on('open', () => {
-		obsConnected = true;
-		console.log('Connected to the OBS WebSocket!');
-	});
+    obsSocket.on('open', () => {
+        obsConnected = true;
+        console.log('Connected to the OBS WebSocket!');
+    });
 
-	obsSocket.on('close', () => {
-		obsConnected = false;
-		console.log('Disconnected from the OBS WebSocket!');
-		setTimeout(initializeObsSocket, 1000);
-	});
+    obsSocket.on('close', () => {
+        obsConnected = false;
+        console.log('Disconnected from the OBS WebSocket!');
+        setTimeout(initializeObsSocket, 1000);
+    });
 
-	obsSocket.on('error', (err) => {
-		console.log(err);
-		setTimeout(initializeObsSocket, 1000);
-	});
+    obsSocket.on('error', (err) => {
+        console.log(err);
+        setTimeout(initializeObsSocket, 1000);
+    });
 
-	obsSocket.on('message', (data) => {
-		var parsedData = JSON.parse(data);
-		if(parsedData['update-type'] == 'SwitchScenes') {
-			var sceneName = parsedData['scene-name'];
-			console.log('Switched to scene ' + sceneName);
+    obsSocket.on('message', (data) => {
+        var parsedData = JSON.parse(data);
+        if(parsedData['update-type'] == 'SwitchScenes') {
+            var sceneName = parsedData['scene-name'];
+            console.log('Switched to scene ' + sceneName);
 
-			if(!qlcConnected) {
-				console.log('QLC is disconnected, ignoring...');
-				return;
-			}
+            if(!qlcConnected) {
+                console.log('QLC is disconnected, ignoring...');
+                return;
+            }
 
-			var buttonId = sceneMappings[sceneName];
+            var buttonId = sceneMappings[sceneName];
 
-			// Stop running show
-			for(var sceneName in sceneMappings) {
-				var currentButtonId = sceneMappings[sceneName];
-				if(buttonId == currentButtonId) {
-					continue;
-				}
-				var currentButton = widgets[currentButtonId];
-				if(currentButton.active) {
-					// Flip
-					qlcSocket.send(currentButtonId + '|255');
-					qlcSocket.send(currentButtonId + '|0');
-					console.log(currentButton.text + ' was active... disabled.');
-				}
-			}
+            // Stop running show
+            for(var sceneName in sceneMappings) {
+                var currentButtonId = sceneMappings[sceneName];
+                if(buttonId == currentButtonId) {
+                    continue;
+                }
+                var currentButton = widgets[currentButtonId];
+                if(currentButton.active) {
+                    // Flip
+                    qlcSocket.send(currentButtonId + '|255');
+                    qlcSocket.send(currentButtonId + '|0');
+                    console.log(currentButton.text + ' was active... disabled.');
+                }
+            }
 
-			// Find new light show
-			if(buttonId != undefined) {
-				var button = widgets[buttonId];
-				if(button != undefined) {
-					console.log('Using light show ' + button.text);
-					if(!button.active) {
-						qlcSocket.send(buttonId + '|255');
-						qlcSocket.send(buttonId + '|0');
-					} else {
-						console.log('The show was already running!');
-					}
-				}
-			}
-		}
-	});
+            // Find new light show
+            if(buttonId != undefined) {
+                var button = widgets[buttonId];
+                if(button != undefined) {
+                    console.log('Using light show ' + button.text);
+                    if(!button.active) {
+                        qlcSocket.send(buttonId + '|255');
+                        qlcSocket.send(buttonId + '|0');
+                    } else {
+                        console.log('The show was already running!');
+                    }
+                }
+            }
+        }
+    });
 };
 
 var qlcSocket;
 var qlcConnected = false;
 
 function initializeQlcSocket() {
-	qlcSocket = new WebSocket(qlcWsAddress);
+    qlcSocket = new WebSocket(qlcWsAddress);
 
-	qlcSocket.on('open', () => {
-		// Update widget status...
-		widgets = {};
-		Request(qlcAddress)
-			.then(html => {
-				var $ = CheerIO.load(html);
-				$('.vcbutton').each((index, entry) => {
-					var style = entry.attribs['style'];
-					var button = {
-						id: entry.attribs['id'],
-						text: $(entry).text(),
-						active: style.includes('border: 3px solid #00E600;'),
-						monitoring: style.includes('border: 3px solid #FFAA00;')
-					};
-					widgets[button.id] = button;
-				});
-				qlcConnected = true;
-				console.log('Connected to the QLC WebSocket!');
-			});
-	});
+    qlcSocket.on('open', () => {
+        // Update widget status...
+        widgets = {};
+        Request(qlcAddress)
+            .then(html => {
+                var $ = CheerIO.load(html);
+                $('.vcbutton').each((index, entry) => {
+                    var style = entry.attribs['style'];
+                    var button = {
+                        id: entry.attribs['id'],
+                        text: $(entry).text(),
+                        active: style.includes('border: 3px solid #00E600;'),
+                        monitoring: style.includes('border: 3px solid #FFAA00;')
+                    };
+                    widgets[button.id] = button;
+                });
+                qlcConnected = true;
+                console.log('Connected to the QLC WebSocket!');
+            });
+    });
 
-	qlcSocket.on('close', () => {
-		if(!qlcConnected) {
-			return;
-		}
-		qlcConnected = false;
-		console.log('Disconnected from the QLC WebSocket!');
-		setTimeout(initializeQlcSocket, 1000);
-	});
+    qlcSocket.on('close', () => {
+        if(!qlcConnected) {
+            return;
+        }
+        qlcConnected = false;
+        console.log('Disconnected from the QLC WebSocket!');
+        setTimeout(initializeQlcSocket, 1000);
+    });
 
-	qlcSocket.on('error', (err) => {
-		if(err.code != 'ECONNREFUSED') {
-			console.log(err);
-		}
-		setTimeout(initializeQlcSocket, 1000);
-	});
+    qlcSocket.on('error', (err) => {
+        if(err.code != 'ECONNREFUSED') {
+            console.log(err);
+        }
+        setTimeout(initializeQlcSocket, 1000);
+    });
 
-	qlcSocket.on('message', (data) => {
-		var splitted = data.split('|');
-		if(splitted[1] == 'BUTTON') {
-			var id = splitted[0];
-			var button = widgets[id];
-			var value = splitted[2];
-			if(button != undefined) {
-			   button.active = value == 255;
-			   button.monitoring = value == 127;
-			   //console.log('Updated button ' + button.text + ' status! Act: ' + button.active + ' Mon: ' + button.monitoring);
-			}
-		}
-	});	
+    qlcSocket.on('message', (data) => {
+        var splitted = data.split('|');
+        if(splitted[1] == 'BUTTON') {
+            var id = splitted[0];
+            var button = widgets[id];
+            var value = splitted[2];
+            if(button != undefined) {
+               button.active = value == 255;
+               button.monitoring = value == 127;
+               //console.log('Updated button ' + button.text + ' status! Act: ' + button.active + ' Mon: ' + button.monitoring);
+            }
+        }
+    });    
 };
 
 // Initialize
